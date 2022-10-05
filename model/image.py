540c50f911dc57983e38fd112128161b6961b384
Anna Fehr <afehr@wesleyan.edu>
2022-10-05 10:00:57 -0400

    Implementing Image.sample()

--- model/image.py
+++ model/image.py
@@ -8,6 +8,9 @@ class Image:
         self.nx = np.shape(val)[1]
         self.ny = np.shape(val)[0]
 
+        if imres:
+            self.imres = imres
+
         if axes:
             self.x = axes[1]
             self.y = axes[0]
@@ -27,9 +30,35 @@ class Image:
     def rotate(self, obs):
         self.val = ndimage.rotate(self.val, 90-obs.PA, reshape=False)
 
-    def sample(self, u, v):
+    def sample(self, vis_temp, u, v, obs, saveim=False):
         # sample image at u, v points
+        data = data_vis[0].data['data']
+        re = ((data[:,0,0,0,:,0,0]).astype(np.float64).copy(order='C'))
+        im = ((data[:,0,0,0,:,0,1]).astype(np.float64).copy(order='C'))
+        w = ((data[:,0,0,0,:,0,2]).astype(np.float64).copy(order='C'))
+        model_vis = np.zeros(data.shape)
+        
         self.square()
+        
+        if not saveim:
+            return chi2Image(self.val, self.imres*np.pi/(180*3600), u, v)
+        
+        vis = sampleImage(self.val, self.imres*np.pi/(180*3600), u, v)
+        
+        for i in range(np.shape(model_vis)[4]):
+            model_vis[:, 0, 0, 0, i, 0, 0] = vis.real
+            model_vis[:, 0, 0, 0, i, 1, 0] = vis.real
+            model_vis[:, 0, 0, 0, i, 0, 1] = vis.imag
+            model_vis[:, 0, 0, 0, i, 1, 1] = vis.imag
+
+        data_vis[0].data['data'] = data_vis[0].data['data'] - model_vis
+        data_vis.writeto(residout, overwrite=True)
+        
+        model_vis[:, 0, 0, 0, :, 0, 2] = data[:, 0, 0, 0, :, 0, 2]  # weights (XX)
+        model_vis[:, 0, 0, 0, :, 1, 2] = data[:, 0, 0, 0, :, 1, 2]  # weights (YY)
+        data_vis[0].data['data'] = model_vis
+        data_vis.writeto(modelout, overwrite=True)
+        data_vis.close()
 
     def save(self, obs):
         hdu = fits.PrimaryHDU(self.val, obs.header(self.nx))
