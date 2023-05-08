86b7318ba67d0b26de1147b7b91efe112fc815f5
Anna Fehr <afehr@wesleyan.edu>
2023-05-08 20:50:30 +0000

    move files

---
+++ debris_disk/src/constants.py
@@ -0,0 +1,21 @@
+from astropy import constants as const
+
+AU      = const.au.cgs.value       # - astronomical unit (cm)
+Rsun    = const.R_sun.cgs.value    # - radius of the sun (cm)
+c       = const.c.cgs.value        # - speed of light (cm/s)
+h       = const.h.cgs.value        # - Planck's constant (erg/s)
+kB      = const.k_B.cgs.value      # - Boltzmann's constant (erg/K)
+sigmaB  = const.sigma_sb.cgs.value # - Stefan-Boltzmann constant (erg cm^-2 s^-1 K^-4)
+pc      = const.pc.cgs.value       # - parsec (cm)
+Jy      = 1.e23                    # - cgs flux density (Janskys)
+Lsun    = const.L_sun.cgs.value    # - luminosity of the sun (ergs)
+Mearth  = const.M_earth.cgs.value  # - mass of the earth (g)
+Msun    = const.M_sun.cgs.value    # - solar mass (g)
+G       = const.G.cgs.value        # - gravitational constant (cm^3/g/s^2)
+rad     = 206264.806               # - radian to arcsecond conversion
+kms     = 1e5                      # - convert km/s to cm/s
+Ghz     = 1e9                      # - Ghz (hz)
+beta    = 1
+c       = const.c.cgs.value        # - speed of light (cm/s)
+h       = const.h.cgs.value        # - Planck's constant (erg/s)
+kB      = const.k_B.cgs.value      # - Boltzmann's constant (erg/K)
---
+++ debris_disk/src/observation.py
@@ -0,0 +1,44 @@
+import debris_disk.constants as const
+from astropy.io import fits
+import numpy as np
+
+
+class Observation:
+    def __init__(self,
+                 nu = 345.8,
+                 imres = 0.005,
+                 distance = 100.,
+                 vis_file=None):
+        if vis_file:
+            pass
+        else:
+            self.nu = nu 
+            self.lamb = 3e8 / nu # wavelength [m]
+            self.imres = imres
+            
+        self.distance = distance
+        self.D = 12. # antennae diameter [m]
+    
+    def header(self, nX):
+        '''
+        Produce Image fits header, assumes that NAXIS1 = NAXIS2 = nX
+        '''
+        hdr = fits.Header()
+        cen = [nX/2.+.5, nX/2.+.5]   # - central pixel location
+
+        hdr['SIMPLE']='T'
+        hdr['BITPIX'] = 32
+        hdr['NAXIS'] = 3
+        hdr['NAXIS1'] = nX
+        hdr['NAXIS2'] = nX
+        hdr['CDELT1'] = -1.*self.imres/3600.
+        hdr['CRPIX1'] = cen[0]
+        hdr['CRVAL1'] = 0
+        hdr['CTYPE1'] = 'RA---SIN'
+        hdr['CDELT2'] = self.imres/3600.
+        hdr['CRPIX2'] = cen[1]
+        hdr['CRVAL2'] = 0
+        hdr['CTYPE2'] = 'DEC--SIN'
+        hdr['OBJECT'] = 'model'
+
+        return hdr
