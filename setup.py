cf67acc20a8f7f550ee6f9b28cec719e8de76871
Anna Fehr <afehr@wesleyan.edu>
2023-05-13 16:56:50 +0000

    checking version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
-setup(
-    name='debris_disk',
-    packages=find_packages(include=['debris_disk']),
+setup(name='debris_disk',
     version='0.1.0',
+    description='Parametric modeling code for debris disks',
+    url='https://github.com/annajfehr/debris_disk/',
+    packages=find_packages(include=['debris_disk']),
     install_requires=[],
     description='Parametric Disk Modeling Code',
-    author='Me',
-    license='MIT',
-)
+    author='Anna Fehr',
+    zip_safe=False)
