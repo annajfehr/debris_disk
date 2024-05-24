checkpoint in fixing units issue

debris_disk/disk.py
@@ -78,9 +78,9 @@ class Disk:
     """
     def __init__(self,
                  L_star=1.,
-                 sigma_crit = 1e-18, # g/cm^3
+                 sigma_crit = 1e-18, # g/cm^2
                  inc=0, # degrees
-                 F_star=0, #microjy
+                 F_star=0, # Jy
                  radial_func='powerlaw',
                  radial_params={'alpha' : 1., 
                                 'Rin' : 10, 
@@ -111,7 +111,7 @@ class Disk:
         inc : float, optional
             Inclination of the disk relative to the viewer [degrees], default is 0. 
         F_star : float
-            Emission from the star [microjy], default is 0
+            Emission from the star [Jy], default is 0
         radial_func : {'powerlaw', 'powerlaw_errf', 'double_powerlaw',
                        'triple_powerlaw'}, optional
             Functional form of the radial profile, default is 'powerlaw'
@@ -145,17 +145,25 @@ class Disk:
         self.max_mem = .75 * memory/16 * 1e9
 
         if obs:
+            #print("obs activated!")
             if type(obs) == dict:
                 obs = Observation(**obs)
             self.obs = obs
-            self.modres = obs.imres * obs.distance * const.AU
+            self.modres = obs.imres * obs.distance * const.AU # modres [cm/pixel]; obs.imres is in arcsec/pixel, obs.distance is in pc, const.AU is cm/AU
+            #print("imres: ", obs.imres)
+            #print("distance: ", obs.distance)
+            #print("const.AU: ", const.AU)
+            #print("modres: ", self.modres)
             # self.max_r = obs.distance * 8 * const.AU
             self.max_r = obs.distance * obs.beam_fwhm * 3600 * 180 / np.pi * const.AU
         else:
+            print("NO obs activated!")
             self.modres = 0.5 * const.AU
             self.max_r = 50 * 8 * const.AU
         if rmax:
-            self.max_r = obs.distance * rmax * const.AU
+            #print("rmax activated!")
+            self.max_r = obs.distance * rmax * const.AU # rmax in cm
+            #print("rmax: ", self.max_r)
 
         if 'Rin' in radial_params:
             if radial_params['Rin'] > radial_params['Rout']:
@@ -165,6 +173,9 @@ class Disk:
         self.L_star = L_star
         self.F_star = F_star
         self.sigma_crit = sigma_crit
+        #print("Lstar: ", self.L_star)
+        #print("Fstar: ", self.F_star)
+        #print("sigmacrit: ", self.sigma_crit)
         
         if inc < 90:
             self.inc = inc * np.pi / 180.
@@ -172,12 +183,15 @@ class Disk:
             self.inc = (180 - inc) * np.pi/180
 
         self.radial_func = radial_func
-        self.radial_params = self.rp_convert(radial_params.copy())
+        self.radial_params = self.rp_convert(radial_params.copy()) # convert params from AU to cm
         
         if rbounds:
             self.rbounds=rbounds.copy()
+            print("rbounds given! ", self.rbounds)
         else:
             self.rbounds = rbounds
+            print("rbounds not given! ", self.rbounds)
+
 
         self.gap = gap
         self.gap_params = gap_params
@@ -196,7 +210,7 @@ class Disk:
         if obs and calc_image:
             self._im(obs) # Integrate to find image
     
-    def rp_convert(self, radial_params):
+    def rp_convert(self, radial_params): # convert relevant params to cm
         def convert_func(func, params):
             if func == 'gaussian':
                 return profiles.gaussian.conversion(params, const.AU)
@@ -240,26 +254,34 @@ class Disk:
         if self.rbounds:
             self.rbounds[0] *= const.AU
             self.rbounds[1] *= const.AU
+            print("rbounds given in struct2d! ", self.rbounds)
         else:
             self._rbounds() # Find radial extent
+            print("found the rbounds! ", self.rbounds)
 
         # Set number of pixels in 2d radial array to 5 times the desired final
         # image resolution
-        self.nr = int(5  * (self.rbounds[1] - self.rbounds[0]) / self.modres) 
+        self.nr = int(5  * (self.rbounds[1] - self.rbounds[0]) / self.modres) # modres in [cm]
 
         # Define radial sampling grid -- for 'powerlaw' profile use logspaced
         # grid, in all other cases use uniform sampling
-        self.r = np.linspace(self.rbounds[0], self.rbounds[1], self.nr)
+        self.r = np.linspace(self.rbounds[0], self.rbounds[1], self.nr) # in [cm]
+        #print("2d structure parameters")
+        #print("self.r: ", self.r)
+        #print("self.nr: ", self.nr)
+        #print("self.rbounds[0]: ", self.rbounds[0])
+        #print("self.rbounds[1]: ", self.rbounds[1])
+        #print("self.modres: ", self.modres)
  
         # Calculates scale height as a function of r
         vert_struct = self.H(**self.vert_params)
 
         self._zmax(vert_struct[0]) # Find vertical extent
-        self.nz = int(5 * self.zmax / self.modres) # 5x final image resolution
+        self.nz = int(5 * self.zmax / self.modres) # 5x final image resolution, modres in [cm]
 
         self.z = np.linspace(0, self.zmax, self.nz)
         
-        rr, zz = np.meshgrid(self.r, self.z)
+        rr, zz = np.meshgrid(self.r, self.z) # rr, zz meshgrid in cm
         assert self._rho2d(rr, zz, vert_struct)  # create 2d disk density structure
         assert self._T2d(rr, zz) # Calculate 2d temperature array
         return True
@@ -273,6 +295,7 @@ class Disk:
         None
         """
         self.rbounds =  [self.modres/2, self.max_r]
+        print("inside _rbounds()")
  
     def H(self, Hc, Rc, psi, gamma=2):
         """
@@ -344,7 +367,9 @@ class Disk:
         """
         
         # Radial x vertical density structure
-        self.rho2d = self.sigma(rr) * self.vert(zz, *vert_struct) 
+        print("radial grid rr:", np.shape(rr))
+        print("radial grid rr:", rr)
+        self.rho2d = self.sigma(rr) * self.vert(zz, *vert_struct) # rr, zz in [cm], rho2d in g/cm^2
         np.nan_to_num(self.rho2d, nan=1e-60) # Check for nans
         return True
 
@@ -363,7 +388,7 @@ class Disk:
         self.r[i]
         """
 
-        def profile_from_func(radial_func, params):
+        def profile_from_func(radial_func, params): # params have been coverted to cm at this point (rr also in cm)
             if radial_func == 'powerlaw':
                 return profiles.powerlaw.val(rr, **params)
             
@@ -425,6 +450,13 @@ class Disk:
             val*=gap
             self.g = gap
 
+        print("radial profile fun times")
+        print(np.shape(val))
+        print(np.mean(val))
+        print(np.max(val))
+        print(np.min(val))
+        print(np.sum(val))
+        print(val)
         self.sigma= self.sigma_crit * val
         return self.sigma_crit * val
 
@@ -474,7 +506,7 @@ class Disk:
         """
         dist = np.sqrt((rr**2) + (zz**2))
         self.dist = dist
-        self.T2d = (self.L_star * const.Lsun / (16. * np.pi * (dist**2) * const.sigmaB))**0.25
+        self.T2d = (self.L_star * const.Lsun / (16. * np.pi * (dist**2) * const.sigmaB))**0.25 # all in cgs
         return True
 
     def incline(self):
@@ -557,19 +589,31 @@ class Disk:
         tau = cumtrapz(self.rho, self.S, axis=2, initial=0.)
         
         for n in obs.nu:
-            kap = 10 * (n/1e12)**const.beta
+            #print("OBS NU INFO")
+            #print(obs.nu)
+            #print(n)
+            kap = 10 * (n/1e12)**const.beta # cm^2/g
             tau *= kap
 
             # Knu_dust = kap*self.rho      # - dust absorbing coefficient
             # Snu = BBF1*n**3/(np.exp((BBF2*n)/self.T)-1.) # - source function
-
+            #print("APPENDING THE IMAGE!!!")
+            #print("obs.imres: ", obs.imres)
+            #print("self.modres: ", self.modres)
+            #print("self.rho mean: ", np.mean(self.rho))
+            #print("self.rho max: ", np.max(self.rho))
+            #print("self.rho shape: ", np.shape(self.rho))
             #arg = kap*self.rho*np.exp(-tau)*BBF1*n**3/(np.exp((BBF2*n)/self.T)-1.) # - source function
             self.ims.append(Image(trapezoid(kap*self.rho*np.exp(-tau)*BBF1*n**3/(np.exp((BBF2*n)/self.T)-1.), 
-                                            self.S, 
+                                            self.S, # integrate over the whole disk (S is the distance from the center of the disk along the observer LOS)
                                             axis=2),
                                   obs.imres,
                                   modres=self.modres))
-            self.ims[-1].add_star(self.F_star)
+            # Initialization of image: input in [erg / (s cm^2 Hz)] but then immediately converted to Jy
+            # Wait is this in [erg / (s cm^2 Hz ster)]? We are integrating over self.S which is the projected distance from center of disk. Maybe that doesn't get rid of the ster like I thought
+            self.ims[-1].add_star(self.F_star) # add the star to the most recent image, also in Jy
+            #print("my star info:")
+            #print(self.F_star)
 
     def square(self):
         """

debris_disk/image.py
@@ -34,28 +34,57 @@ class Image:
     """
 
     def __init__(self, val, imres=None, axes=None, modres=None):
-        self.val = val * 1e23 
+        print("in image.py, val")
+        print(np.mean(val)) # erg / (cm^2 s Hz) ?
+        self.val = val * 1e23 # Jy/ster
+        print("in image.py, val2 (possibly Jy/ster?)")
+        # per pixel?
+        print(np.mean(self.val))
         
         self.nx = np.shape(val)[1]
         self.ny = np.shape(val)[0]
 
         if modres:
-            self.val *= modres*modres
+            print("modres is being passed in")
+            print(modres)
+            print(np.mean(self.val))
+            #self.val *= modres*modres # why would we do this? would be some weird unit
+            #print(modres*modres)
+            #self.val /= modres*modres # this would convert to Jy/cm^2 (I think?)
+            #print("now in Jy/cm^2????")
+            #self.val /= imres*imres
+            # I DON'T KNOW WHAT THE UNITS OF THE IMAGE SHOULD BE
+            # I think they are already in Jy/pixel so what if I just...comment this out?
+            self.val *= ((imres**2) / (const.rad)**2) # if started with Jy/ster, now in Jy/pixel?
+            print("did i fix it? let's see idk")
+            print(np.mean(self.val))
         if imres:
             self.imres = imres
+            print("imres is being passed in")
+            print(self.imres)
 
         if axes:
-            self.x = axes[0]
-            self.y = axes[1]
+            self.x = axes[0] # array of x pixel values in arcsec
+            self.y = axes[1] # array of y pixel values in arcsec
         else:
             self._axes()
+        print("self x and y")
+        print(self.x)
+        print(self.y)
+        print(np.shape(self.x))
+        print(np.shape(self.y))
+        print("nx and ny")
+        print(self.nx)
+        print(self.ny)
 
     def _axes(self):
         start = int(self.nx/2)
         self.x = self.imres*(np.linspace(-start, start-1, self.nx))
+        print("start: ", start)
         
         start = int(self.ny/2)
         self.y = self.imres*(np.linspace(-start, start-1, self.ny))
+        print("start: ", start)
 
     def square(self):
         if self.nx == self.ny:
@@ -99,4 +128,5 @@ class Image:
         self.val *= self.beam
 
     def add_star(self, f_star):
-        self.val[int(self.ny/2), int(self.nx/2)] += f_star / 1e6
+        # add star at center of image
+        self.val[int(self.ny/2), int(self.nx/2)] += f_star # in Jy

debris_disk/observation.py
@@ -17,7 +17,11 @@ class Observation:
                  D=12):
         if vis:
             self.nu = const.c / vis.chans
-            self.imres = min([np.min(res*(180/np.pi) * 3600/2) for res in vis.resolution])
+            print("visbility resolutions!")
+            print(vis.resolution)
+            self.imres = min([np.min(res*(180/np.pi) * 3600/2) for res in vis.resolution]) # in arcseconds
+            print("new imres")
+            print(self.imres)
         else:
             self.nu = nu 
             self.imres=imres

debris_disk/uvdata.py
@@ -9,7 +9,11 @@ class UVDataset:
     def __init__(self, f=None, stored=None, filetype='txt', freq=3.380857111374e11):
         # frequency is in Hz
         if stored:
+            print("HOLD YOUR PONIES LET'S CHECK OUT WHAT IS BEING STORED")
+            print(stored)
             for key in stored:
+                print(key)
+                print(stored[key])
                 setattr(self, key, stored[key])
             return
 
@@ -19,9 +23,9 @@ class UVDataset:
         for file in f:
             self.datasets.append(UVData(file, filetype=filetype))
 
-        self._mrs()
+        self._mrs() # maximum recoverable scale for the whole dataset
         self._resolution()
-        self.chans = [ds.chans for ds in self.datasets]
+        self.chans = [ds.chans for ds in self.datasets] # yeah so unfortunately in the code's current form, this could be either lambda [cm] or freq [Hz]
         self.freq = freq # frequency of obs in Hz
 
     def _mrs(self):
@@ -51,31 +55,36 @@ class UVDataset:
         
         chi2=0
 
-        images = disk.ims
+        images = disk.ims # the images we generated when the model Disk was initialized
         
 
         for i, ds in enumerate(self.datasets): # so ds is a single UVData object
-            chi2+=ds.chi2(ims=[images[i]], dxy=dxy, disk=disk, PA=PA, dRA=dRA, dDec=dDec, F_star=F_star)
-            #images = images[1:] # okay either have this line and images[0], or omit this line and have images[i]
+            chi2+=ds.chi2(ims=[images[0]], dxy=dxy, disk=disk, PA=PA, dRA=dRA, dDec=dDec, F_star=F_star)
+            images = images[1:] # okay either have this line and images[0], or omit this line and have images[i]
         return chi2
 
 
 class UVData:
     def __init__(self, f=None, stored=None, filetype='txt', freq=3.380857111374e11):
         if stored:
+            print("HOLD YOUR HORSES LET'S CHECK OUT WHAT IS BEING STORED")
+            print(stored)
             for key in stored:
+                print(key)
+                print(stored[key])
                 setattr(self, key, stored[key])
             return
         self.file = f
         self.filetype=filetype
         self.freq = freq
 
-        if filetype == 'txt':
+        if filetype == 'txt': # how we actually read in data to begin modeling
             self.u, self.v, self.real, self.imag, self.w, self.lams = np.require(np.loadtxt(f, unpack=True), requirements='C')
-            self.chans  = np.unique(self.lams)[0] # the lambda values in [m], actually this is only one of them. Don't wanna change it and break everything
+            self.chans  = np.unique(self.lams)[0] # the lambda values in [m], actually this is only one of them. Don't wanna change it and break everything. RETURN TO THIS LATER
             self.chans *= 100 # the lambda values in [cm]
 
         if filetype == 'fits':
+            print("Warning, you're in the fits zone of UVData in uvdata.py! We need to check for unit issues here!")
             data = fits.open(f)[0]
             self.chans = np.arange(0, data.header['NAXIS5']) * data.header['CDELT4'] + data.header['CRVAL4']
             self.u = np.array([data.data['UU'] * chan for chan in self.chans]).astype(np.float64)
@@ -93,21 +102,22 @@ class UVData:
             self.w = self.w.byteswap().newbyteorder().squeeze()
             self.w = np.swapaxes(self.w, 0, 1).astype(np.float64)
             
-            self.chans = [2.998e10/chan for chan in self.chans]
+            self.chans = [const.c/chan for chan in self.chans] # this assumes that chan in self.chans is in cm
             self.data = fits.open(f)
 
-        self._mrs()
+        self._mrs() #maximum recoverable scale for this UVData
         self._resolution()
 
         #self.nchans = len(self.chans)
 
-    def _mrs(self):
+    def _mrs(self): # get the maximum recoverable scale for this set of uv data
         self.mrs = find_mrs(self.u, self.v)
 
     def _resolution(self):
         self.resolution = find_resolution(self.u, self.v)
     
     def sample(self, ims=None, dxy=None, disk=None, modout='mod', PA=0., dRA=0., dDec=0., F_star=0, imchecked=False):
+        print("INSIDE sample()")
         PA *= np.pi /180 # PA is now in RADIANS!
         dRA *= np.pi /(180*3600) 
         dDec *= np.pi/(180*3600) 
@@ -116,10 +126,15 @@ class UVData:
 
         for i, im in enumerate(ims):
             val, dxy = prepare_image(im, self.mrs, self.freq)#self.chans)
-            
+            print("sample stuff")
+            print("image: ", np.mean(val))
+            print(dxy)
+            print(self.u)
             if self.filetype=='txt':
-                Vmodel = gd.sampleImage(val,
-                                    dxy,
+                # get complex array of model visibilities
+                print("about to run Galario sampleImage()")
+                Vmodel = gd.sampleImage(val, # [Jy/pixel]
+                                    dxy, # [rad]
                                     self.u, self.v,
                                     dRA=dRA, 
                                     dDec=dDec, 
@@ -135,6 +150,7 @@ class UVData:
 
 
     def chi2(self, ims=None, dxy=None, disk=None, PA=0., dRA=0., dDec=0., F_star=0, imchecked=False):
+        print("INSIDE chi2()")
         chi2 = 0
         PA *= np.pi /180 # PA IS IN RADIANS
         dRA *= np.pi /(180*3600) 
@@ -142,21 +158,28 @@ class UVData:
 
         if ims == None:
             assert len(disk.ims) == len(self.re)
-            ims = disk.ims
+            ims = disk.ims # get the model images from the model disk
 
         gd.threads(num=1)
         Vstar=F_star*np.exp(2*np.pi*1j*(self.u*dRA +self.v*dDec))
+        print("F_star: ", F_star)
+        print("Vstar: ", Vstar)
 
         for i, im in enumerate(ims):
-            val, dxy = prepare_image(im, self.mrs, self.freq)#self.chans)
+            val, dxy = prepare_image(im, self.mrs, self.freq)#self.chans) # apply primary beam correction and get dxy in radians
             if self.filetype=='txt':
-                chi2 += gd.chi2Image(val, 
-                                     dxy, 
-                                     self.u, 
-                                     self.v, 
-                                     self.real-Vstar.real,
-                                     self.imag-Vstar.imag,
-                                     self.w, 
+                print("chi2 stuff")
+                print("image: ", np.mean(val))
+                print(dxy)
+                print(self.u)
+                print("About to run Galario chi2Image (for txt)")
+                chi2 += gd.chi2Image(val, # model image, in units of [Jy/pixel]
+                                     dxy, # size of square image cell/pixel in [rad] (generated in prepare_image())
+                                     self.u, # u visibility coordinates (in units of wavelength, as read in from the text files)
+                                     self.v, # v visibility coordinates (in units of wavelength, as read in from the text files)
+                                     self.real-Vstar.real, # real component of vis [Jy], as read in from the text files
+                                     self.imag-Vstar.imag, # imaginary component of vis [Jy], as read in from the text files
+                                     self.w, # visibility weights, as read in from the text files
                                      PA=(np.pi/2+PA),
                                      dRA=dRA,
                                      dDec=dDec,
@@ -167,6 +190,7 @@ class UVData:
                 w = self.w[i,:,0]
                 #im = self.im[i,:,0].byteswap().newbyteorder().squeeze()
                 #w = self.w[i,:,0].byteswap().newbyteorder().squeeze()
+                print("About to run Galario chi2Image (for fits)")
                 chi2 += gd.chi2Image(val, 
                                      dxy, 
                                      self.u[i].copy(order='C'), 
@@ -181,6 +205,7 @@ class UVData:
                 re = self.re[i,:,1].byteswap().newbyteorder().squeeze()
                 imag = self.im[i,:,1]
                 w = self.w[i,:,1]
+                print("About to run Galario chi2Image (for fits again for some reason)")
                 chi2 += gd.chi2Image(val, 
                                      dxy, 
                                      self.u[i].copy(order='C'), 
@@ -197,11 +222,14 @@ class UVData:
 
 
 def prepare_image(im, mrs, nu):
-    dxy = im.imres * np.pi /(180*3600) 
+    print("prepare_image imres: ", im.imres)
+    dxy = im.imres * np.pi /(180*3600) # imres in arcsec to dxy [rad]
     
     im.square()
     im.beam_corr(nu, 12)
     val = im.val[::-1,:].copy(order='C')
+    print("image vals: ", np.shape(val))
+    print("image vals: ", np.mean(val))
     min_pixels = int(2 * mrs / dxy)+1
 
     if min_pixels % 2 != 0:
@@ -234,19 +262,20 @@ def prepare_val(val, dxy, mrs, nu, imres):
 
     return val
 
-def find_mrs(u, v):
+def find_mrs(u, v): # maximum recoverable scale (comes from shortest baseline)
     if type(u[0]) == float:
         uvdist = np.hypot(u, v)
     else:
         uvdist = [np.min(np.hypot(up, vp)) for up, vp in zip(u, v)]
-    return 0.6 / np.min(uvdist)
+    return 0.6 / np.min(uvdist) # see Eqn 3.28 in the ALMA technical handbook (Cycle 11)
 
-def find_resolution(u, v):
+def find_resolution(u, v): # angular resolution (comes from longest baseline)
     if type(u[0]) == float:
         uvdist = np.hypot(u, v)
     else:
         uvdist = [np.max(np.hypot(up, vp)) for up, vp in zip(u, v)]
-    return 1 / (2 * np.max(uvdist))
+    print("RESOLUTION IN FIND_RESOLUTION: ", 1 / (2 * np.max(uvdist)))
+    return 1 / (2 * np.max(uvdist)) # see Eqn 3.27 in the ALMA technical handbook (Cycle 11)
 
 def fill_in(val, min_pixels):
     nx = np.shape(val)[0]
@@ -285,6 +314,7 @@ def chiSq(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, PA=0, F_star
             return txt_chiSq(df, modfile, fileout, dxy, dRA, dDec, PA, F_star, residual)
 
 def txt_chiSq(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, PA=0, F_star=0, residual=False, imres=0.005, freq=3.380857111374e11):
+    print("INSIDE txt_chiSq()")
     image=modfile[0].data.astype('double') # important for Galario sometimes
     dpix_deg=modfile[0].header['CDELT2']
     dpix_rad=dpix_deg*np.pi/180.
@@ -294,11 +324,16 @@ def txt_chiSq(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, PA=0, F_
 
     image = prepare_val(image, dpix_rad, mrs, freq, imres)
     Vstar=F_star*np.exp(2*np.pi*1j*(u*dRA +v*dDec))
+    print("text chi squared stuff")
+    print("image: ", np.mean(image))
+    print(dpix_rad)
+    print(u)
 
     if fileout:
-        Vmodel = gd.sampleImage(image,
-                            dpix_rad,
-                            u, v,
+        print("About to run Galario sampleImage")
+        Vmodel = gd.sampleImage(image, # must be in Jy/pixel
+                            dpix_rad, # must be in radians
+                            u, v, # must be dimensionless (in units of observing wavelength)
                             dRA=dRA, 
                             dDec=dDec, 
                             PA=PA+np.pi/2.0,
@@ -316,6 +351,7 @@ def txt_chiSq(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, PA=0, F_
     
     # compute chi2
     # subtract fstar first from observed visibilities
+    print("About to run Galario chi2Image")
     chi2 = gd.chi2Image(image, dpix_rad, u, v,
                      Vreal-Vstar.real,
                      Vimag-Vstar.imag,
@@ -327,6 +363,7 @@ def txt_chiSq(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, PA=0, F_
     return chi2
 
 def fits_chiSq(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, PA=0, F_star=0, residual=False):
+        print("INSIDE fits_chiSq")
         model_fits = fits.open(modfile) # I now open the model fits file
 
         if dxy == None:
@@ -362,6 +399,7 @@ def fits_chiSq(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, PA=0, F
 
                 image = np.require(image, requirements='C') # and this is from the galario documentation -- you get some stupid
 
+                print("About to run Galario sampleImage (for fits)")
                 vis = gd.sampleImage(image[:,:], dxy, u, v, dRA = dRA, dDec = dDec, PA=PA+np.pi/2.0, origin='lower') 
                 vis = vis + Vstar
 
@@ -394,6 +432,7 @@ def fits_chiSq(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, PA=0, F
         return chi
 
 def chiSqStack(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, residual=False):
+        print("INSIDE chiSqStack")
         if type(datafile) == list:
             datafile=datafile[0]
 
@@ -431,7 +470,7 @@ def chiSqStack(datafile, modfile, fileout=None, dxy=None, dRA=0, dDec=0, residua
                 freq = freq_start + i * delta_freq
                 u, v = (data_vis[0].data['UU'] * freq).astype(np.float64), (data_vis[0].data['VV'] * freq).astype(np.float64)
                 foo = model_cor # this is just the corrected model image which i renamed for some weird reason
-
+                print("About to run Galario sampleImage (for fits)")
                 vis = gd.sampleImage(model_cor[:,:], dxy, u, v, dRA = dRA, dDec = dDec, PA=PA+np.pi/2.0, origin='lower') #add stuff from seba here?
 
                 model_vis[:,0,0,i,0,0,0] = vis.real